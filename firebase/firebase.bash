d3e0c9ba530419d46bf52b1978ea0a7ea9f94cf2
anargu <anthony.arostegui@gmail.com>
2021-12-25 03:55:31 -0500

    wip modify firebase

--- firebase/firebase.bash
+++ firebase/firebase.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ -n "$NEXT_PUBLIC_CONFIG" ]; then
+    echo "NEXT_PUBLIC_CONFIG received"
+    echo "$NEXT_PUBLIC_CONFIG"
+fi
+
 # run the original firebase
 if [ $FIREBASE_TOKEN ]; then
-  firebase "$@" --token $FIREBASE_TOKEN
+  NEXT_PUBLIC_CONFIG=$NEXT_PUBLIC_CONFIG firebase "$@" --token $FIREBASE_TOKEN
 else
   firebase "$@"
 fi
